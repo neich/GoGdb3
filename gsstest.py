b0015b54bba0b6736db90cfd08a799ddab879474
Centny <centny@gmail.com>
2014-04-22 12:18:45 +0800

    fix the on save path error.

--- gsstest.py
+++ gsstest.py
@@ -83,6 +83,7 @@ class OnSaveListener(sublime_plugin.EventListener):
 		pkg_n=pkg_pathv(ppath,view)
 		if pkg_n=="":
 			return
+		apath=view.file_name()
 		imain=apath.find("main")==len(apath)-4
 		if imain:
 			return
